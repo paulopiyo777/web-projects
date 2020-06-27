c7f05c1b54ade7b0f90df0584a310febe2d4db80
Paul <paulopiyo777@gmail.com>
2020-06-26 22:55:42 -0500

    add js file and more styles

---
+++ 005_ecommerce-website/js/main.js
@@ -0,0 +1,10 @@
+// first slider
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+  autoplay:true,
+  autoplaySpeed:3000,
+  dots:true,
+  prevArrow:".site-slider .slider-btn .prev",
+  nextArrow:".site-slider .slider-btn .next",
+}); // wraps all items in a single row
